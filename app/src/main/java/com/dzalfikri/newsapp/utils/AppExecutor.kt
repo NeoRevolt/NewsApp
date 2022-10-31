67459d717de4fa9214482113b72d5fd3084b511e
NeoRevolt <dzalfikriali@gmail.com>
2022-10-31 21:00:31 +0700

    Comment some unused lines

--- app/src/main/java/com/dzalfikri/newsapp/utils/AppExecutor.kt
+++ app/src/main/java/com/dzalfikri/newsapp/utils/AppExecutor.kt
@@ -7,8 +7,8 @@ import java.util.concurrent.Executors
 
 class AppExecutor {
     val diskIO: Executor = Executors.newSingleThreadExecutor()
-    val networkIO: Executor = Executors.newFixedThreadPool(3)
-    val mainThread: Executor = MainThreadExecutor()
+//    val networkIO: Executor = Executors.newFixedThreadPool(3)
+//    val mainThread: Executor = MainThreadExecutor()
 
     private class MainThreadExecutor : Executor {
         private val mainThreadHandler = Handler(Looper.getMainLooper())
